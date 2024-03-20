270fb0ec23bfaca0539b09c372d0bc78b2a48f9f
TodescoAmandine <todescoamandine@yahoo.fr>
2024-03-20 13:00:30 +0100

    Modif tags

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import logements from '../logements.json';
+//import logements from '../logements.json';
 
-// const Tags = () => {
-//     const id ='0';
-//     const logement = logements.find(logement => logement.id===id);
-//     return (
-//         <>
-//             {logement.tags.map((tag, index) => (
-//                 <div key={index} className='details__content--tag'>{tag}</div>
-//             ))}
-//         </>
-//     );
-// };
-
-// export default Tags;
 const Tags = ({ tags }) => (
     <div className='tags-row'>
       {tags.map((tag, index) => (
